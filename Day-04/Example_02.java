2da9afe98ff25930ff4a7daf656cbd80fd7d562d
ratulhasansaion-debug <ratulhasansaion@gmail.com>
2026-04-26 06:22:51 +0600

    Create Example_02.java

---
+++ Day-04/Example_02.java
@@ -0,0 +1,25 @@
+class Vehicle {
+    void start() {
+        System.out.println("Vehicle started");
+    }
+}
+class Car extends Vehicle {
+    void drive() {
+        System.out.println("Car is driving");
+    }
+}
+class Bike extends Vehicle {
+    void ride() {
+        System.out.println("Bike is riding");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.start();
+        myCar.drive();
+        Bike myBike = new Bike();
+        myBike.start();
+        myBike.ride();
+    }
+}
