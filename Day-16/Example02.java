642296480e66676afe2dd64e28e49c3878bfc938
ratulhasansaion-debug <ratulhasansaion@gmail.com>
2026-05-10 23:52:44 +0600

    Create Example02.java

---
+++ Day-16/Example02.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("output.bin")) {
+            int i;
+            while ((i = fis.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
